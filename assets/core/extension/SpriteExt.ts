ca22f5591a569e6146fc9164cbe32ff4d13ab071
ChirsDing <334174355@qq.com>
2024-09-21 20:59:17 +0800

    sprite 加载资源管理优化

--- assets/core/extension/SpriteExt.ts
+++ assets/core/extension/SpriteExt.ts
@@ -2,10 +2,14 @@ import { GameBundle } from "@game/common/config/GameBundle";
 import { VMEnv } from "@oops/assets/libs/model-view/VMEnv";
 import { GameComponent } from "@oops/assets/module/common/GameComponent";
 import { js, Sprite, SpriteFrame } from "cc";
+import { oops } from "../Oops";
+import { NodeEventType } from "cc";
+import { EDITOR } from 'internal:constants';
+
 declare module "cc" {
     // 这里使用 interface 进行扩展，如果使用 class 则会与现有的 d.ts 有冲突
     export interface Sprite {
-        setResID(gameComponent: GameComponent, path: string): void;
+        setResID(path: string, bundle?:String): void;
     }
 }
 
@@ -16,16 +20,67 @@ if (!VMEnv.editor) {
         Sprite.prototype["$__definedProperties__"] = true;
 
         js.mixin(Sprite.prototype, {
-            async setResID(gameComponent: GameComponent, path: string) {
-                if (!path.endsWith('/spriteFrame')) {
-                    path += 'spriteFrame';
-                }
-                let retSF = await gameComponent.loadAsync(GameBundle.Bundle, path, SpriteFrame);
-                if (!this.isValid) {
+            resId: "",
+            bundle: "",
+            async setResID(path: string, bundle?:string) {
+                if (this.resId === path && this.bundle === bundle) {
                     return;
                 }
+                let lastResId = this.resId;
+                let lastBundle = this.bundle;
+
+                this.resId = path;
+                this.bundle = bundle || GameBundle.Bundle;
+
+                let retSF = await oops.res.loadAsync(this.bundle, path, SpriteFrame);
+                if (!retSF) {
+                    return;
+                }
+                
+                retSF.addRef();
+                if (!this.isValid || this.resId !== path || this.bundle !== bundle) {
+                    // 如果在加载过程中，资源已经被释放了，就不再设置了
+                    retSF.decRef();
+                    return;
+                }
+
                 this.spriteFrame = retSF;
-            }
+                this.releaseRes(lastResId, lastBundle);
+
+            },
+            
+            releaseRes(path: string, bundle:string) {
+                this.spriteFrame = null;
+                let retSF = oops.res.get(path, SpriteFrame, bundle);
+                if (retSF) {
+                    retSF.decRef();
+                }
+            },
+
+            onDestroy() {
+                if (EDITOR) {
+                    this.node.off(NodeEventType.SIZE_CHANGED, this._resized, this);
+                }
+                
+                if (this.resId && this.bundle) {
+                    this.spriteFrame = null;
+                    this.releaseRes(this.resId, this.bundle);
+                    this.resId = "";
+                    this.bundle = "";
+                }
+
+                this._renderEntity.setNode(null);
+                if (this.node._uiProps.uiComp === this) {
+                    this.node._uiProps.uiComp = null;
+                }
+                this.destroyRenderData();
+                if (this._materialInstances) {
+                    for (let i = 0; i < this._materialInstances.length; i++) {
+                        const instance = this._materialInstances[i];
+                        if (instance) { instance.destroy(); }
+                    }
+                }
+            },
         });
     }
 }
